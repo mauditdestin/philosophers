d3a46b91a499242fb01c5d09b399c99597452d88
catlover4242 <akapaui@protonmail.com>
2025-05-02 12:47:20 +0200

    + | Added philo_routine, philo_died, thread etc.

--- inc/philosophers.h
+++ inc/philosophers.h
@@ -25,14 +25,16 @@
 
 typedef struct s_philo
 {
-	pthread_t	thread;
-	int			id;
-	int			num_of_philos;
-	size_t		time_to_die;
-	size_t		time_to_eat;
-	size_t		time_to_sleep;
-	size_t		start_time;
-	int			num_times_of_eat;
+	pthread_t		thread;
+	int				id;
+	int				num_of_philos;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			start_time;
+	int				num_times_of_eat;
+	pthread_mutex_t	*dead_lock;
+	bool			dead;
 }	t_philo;
 
 typedef struct s_program
@@ -47,3 +49,4 @@ size_t	get_current_time(void);
 
 // ROUTINE
 void	*monitor_routine(void *arg);
+void	philo_routine(void *arg);
--- srcs/routine.c
+++ srcs/routine.c
@@ -12,6 +12,32 @@
 
 #include "../inc/philosophers.h"
 
+bool	philo_died(t_philo *philo)
+{
+	bool	dead;
+
+	pthread_mutex_lock(philo->dead_lock);
+	dead = *philo->dead;
+	pthread_mutex_unlock(philo->dead_lock);
+	return (dead);
+}
+
+void	philo_routine(void *arg)
+{
+	t_philo	*philos;
+
+	philos = (t_philo *)arg;
+	if (philos->id % 2 == 0)
+		ft_usleep(NULL, 1);
+	while (philo_died(philos) == false)
+	{
+		philo_eat(philos);
+		philo_sleep(philos);
+		philo_think(philos);
+	}
+	return (NULL);
+}
+
 void	*monitor_routine(void *arg)
 {
 	t_philo *philos;
