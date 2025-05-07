d7143b3dee3a361c7efb9cc816b72caaf420e05f
catlover4242 <akapaui@protonmail.com>
2025-05-07 13:28:45 +0200

    + | Added Errors in input.c

--- srcs/input.c
+++ srcs/input.c
@@ -10,3 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/philosophers.h"
+
+static bool		is_arg_count_valid(int argc)
+{
+	return (argc == 5 || argc == 6);
+}
+
+static bool		is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+static bool		is_numb(char *str)
+{
+	if (is_digit(*str) == true || *str == '+' || *str == '-')
+		str++;
+	else
+		return (false);
+	while (*str)
+		if (is_digit(*str++) == false)
+				return (false);
+	return (true);
+}
+
+static bool		is_valid_philo_cfg(int num, int index)
+{
+	if (index == 1 && !(num > 0 && num <= PHILO_MAX))
+		return (print_error(INV_NUM_PHILOS), false);
+	if (index == 2 && !(num > 0 && num <= MAX_VAL))
+		return (print_error(INV_TIME_DIE), false);
+	if (index == 3 && !(num > 0 && num <= MAX_VAL))
+		return (print_error(INV_TIME_EAT), false);
+	if (index == 4 && !(num > 0 && num <= MAX_VAL))
+		return (print_error(INV_TIME_SLEEP), false);
+	if (index == 5 && !(num > 0 && num <= MAX_VAL))
+		return (print_error(INV_NUM_MEALS), false);
+	return (true);
+}
