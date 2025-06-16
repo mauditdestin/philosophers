5599bc4a2f6c133fc4cae8977497e2f89e76f138
catlover4242 <akapaui@protonmail.com>
2025-06-16 13:21:51 +0200

    + | Added programs init

--- srcs/init.c
+++ srcs/init.c
@@ -6,15 +6,19 @@
 /*   By: pleblond <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 14:08:55 by pleblond          #+#    #+#             */
-/*   Updated: 2025/05/09 01:34:14 by pleblond         ###   ########.fr       */
+/*   Updated: 2025/06/16 13:19:01 by pleblond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
 
-void	init_prog(t_program *prog, t_philo *philo)
+void	init_prog(t_program *program, t_philo *philo)
 {
-	
+	program->dead_flag = false;
+	pthread_mutex_init(&program->write_lock, NULL);
+	pthread_mutex_init(&program->dead_lock, NULL);
+	pthread_mutex_init(&program->meal_lock, NULL);
+	program->philos = philo;
 }
 
 void	init_forks(pthread_mutex_t *forks, int num_of_philos)
